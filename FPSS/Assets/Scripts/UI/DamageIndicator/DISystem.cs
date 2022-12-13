33e7377b8cea14b1dfd71a076beb4d6b43ec35c6
Your Name <82323846+dungs072@users.noreply.github.com>
2022-12-13 23:58:45 +0700

    create damage indicator
    dont understand much

---
+++ FPSS/Assets/Scripts/UI/DamageIndicator/DISystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class DISystem : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private DamageIndicator indicatorPrefab = null;
+    [SerializeField] private RectTransform holder = null;
+    [SerializeField] private new Camera camera = null;
+    [SerializeField] private Transform player = null;
+
+    private Dictionary<Transform,DamageIndicator> Indicators = new Dictionary<Transform, DamageIndicator>();
+    #region Delegates
+    public static Action<Transform> CreateIndicator = delegate{};
+    public static Func<Transform,bool> CheckIfObjectInsight = null;
+    #endregion
+
+    private void OnEnable() {
+        CreateIndicator+=Create;
+        CheckIfObjectInsight+=InSight;
+    }
+    private void OnDisable() {
+        CreateIndicator-=Create;
+        CheckIfObjectInsight-=InSight;
+    }
+    private void Create(Transform target)
+    {
+        if(Indicators.ContainsKey(target))
+        {
+            Indicators[target].Restart();
+            return;
+        }
+        DamageIndicator newIndicator = Instantiate(indicatorPrefab,holder);
+        newIndicator.Register(target,player,new Action(()=>{Indicators.Remove(target);}));
+
+        Indicators.Add(target,newIndicator);
+    }
+    private bool InSight(Transform t)
+    {
+        Vector3 screenPoint = camera.WorldToViewportPoint(t.position);
+        return(screenPoint.z>0&&screenPoint.x>0&&screenPoint.x<1&&
+            screenPoint.y>0&&screenPoint.y<1);
+    }
+}
---
+++ FPSS/Assets/Scripts/UI/DamageIndicator/DamageIndicator.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageIndicator : MonoBehaviour
+{
+    private const float MaxTimer = 8.0f;
+    private float timer = MaxTimer;
+
+    private CanvasGroup canvasGroup = null;
+    protected CanvasGroup CanvasGroup
+    {
+        get
+        {
+            if(canvasGroup==null)
+            {
+                canvasGroup = GetComponent<CanvasGroup>();
+                if(canvasGroup==null)
+                {
+                    canvasGroup = gameObject.AddComponent<CanvasGroup>();
+                }
+            }
+            return canvasGroup;
+        }
+    }
+    private RectTransform rect = null;
+    protected RectTransform Rect
+    {
+        get
+        {
+            if(rect==null)
+            {
+                rect = GetComponent<RectTransform>();
+                if(rect==null)
+                {
+                    rect = gameObject.AddComponent<RectTransform>();
+                }
+            }
+            return rect;
+        }
+    }
+
+    public Transform Target{get;private set;} = null;
+    private Transform player = null;
+    private IEnumerator countDownCoroutine = null;
+    private Action unRegister = null;
+
+    private Quaternion tRot = Quaternion.identity;
+    private Vector3 tPos = Vector3.zero;
+
+    public void Register(Transform target, Transform player,Action unRegister)
+    {
+        this.Target = target;
+        this.player = player;
+        StartCoroutine(RotateToTarget());
+        StartTimer();
+    }
+    private void StartTimer()
+    {
+        if(countDownCoroutine!=null)
+        {
+            StopCoroutine(countDownCoroutine);
+        }
+        countDownCoroutine = CountDown();
+        StartCoroutine(countDownCoroutine);
+    }
+    public void Restart()
+    {
+        timer = MaxTimer;
+        StartTimer();
+    }
+    private IEnumerator CountDown()
+    {
+        while(CanvasGroup.alpha<1.0f)
+        {
+            CanvasGroup.alpha+=4*Time.deltaTime;
+            yield return null;
+        }
+        while(timer>0f)
+        {
+            timer--;
+            yield return new WaitForSeconds(1f);
+        }
+        while(CanvasGroup.alpha>1f)
+        {
+            canvasGroup.alpha-= 2*Time.deltaTime;
+            yield return null;
+        }
+        unRegister();
+        Destroy(gameObject);
+    }
+
+    private IEnumerator RotateToTarget()
+    {
+        while(this.enabled)
+        {
+            if(Target)
+            {
+                tPos = Target.position;
+                tRot = Target.rotation;
+            }
+            Vector3 direction = player.position-tPos;
+            tRot = Quaternion.LookRotation(direction);
+            tRot.z = -tRot.y;
+            tRot.y = 0;
+
+            Vector3 northDirection = new Vector3(0,0,player.eulerAngles.y);
+            Rect.localRotation = tRot*Quaternion.Euler(northDirection);
+
+            yield return null;
+        }
+    }
+}
